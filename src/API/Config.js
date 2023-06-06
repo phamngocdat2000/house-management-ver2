0373e14191c593b4b53c9fac9673d8be771b3652
Phạm Ngọc Đạt <79707255+phamngocdat2000@users.noreply.github.com>
2023-06-06 23:23:26 +0700

    Update Config.js

--- src/API/Config.js
+++ src/API/Config.js
@@ -1,5 +1,5 @@
 const config = {
-    HOST: "http://35.247.146.22:8080",
-    WS: "http://35.247.146.22:8080/ws"
+    HOST: "http://35.247.146.225:8080",
+    WS: "http://35.247.146.225:8080/ws"
 };
 export default config;
